a2ff5722dcec03cace09b8f55c35d0c2015f942d
Thodoris <t.foteinakis@intale.com>
2018-05-23 00:50:28 +0300

    Order functionality, some font-awesome icons included

--- client/src/WhiskyGrid/WhiskyGrid.js
+++ client/src/WhiskyGrid/WhiskyGrid.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import WhiskyGridRow from './WhiskyGridRow.js';
 import DefaultRow from './DefaultRow.js';
 import { Table, Badge } from 'reactstrap';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import IconSort from '@fortawesome/fontawesome-free-solid/faSort'
+import IconSortUp from '@fortawesome/fontawesome-free-solid/faSortUp'
+import IconSortDown from '@fortawesome/fontawesome-free-solid/faSortDown'
 import '.././style.css';
 
 
@@ -9,10 +13,13 @@ class WhiskyGrid extends Component {
     constructor(props){
         super(props);
         this.state = {
-            whiskies: []
+            whiskies: [],
+            sorting: ""
         };
         this.onAdd = this.onAdd.bind(this);
         this.onRemove = this.onRemove.bind(this);
+        this.sortBy = this.sortBy.bind(this);
+        this.compareBy = this.compareBy.bind(this);
     };
     
     componentDidMount() {
@@ -52,6 +59,30 @@ class WhiskyGrid extends Component {
         });
     }
 
+    compareBy(key, sorting) {
+        let multiplier = 1;
+
+        if (sorting === "desc") {
+            multiplier = -1;
+        }
+        
+        return function (a, b) {
+            if (a[key] < b[key]) return multiplier * (-1);
+            if (a[key] > b[key]) return multiplier * 1;
+            return 0;
+        };  
+    }
+     
+    sortBy(key) {
+        const newSorting = this.state.sorting === "" ? "asc" : (this.state.sorting === "asc" ? "desc" : "");
+        if (newSorting === "") {
+            key = "Prod_ID";
+        }
+        let arrayCopy = [...this.state.whiskies];
+        arrayCopy.sort(this.compareBy(key, newSorting));
+        this.setState({whiskies: arrayCopy, sorting: newSorting});
+    }
+
     render() {
         const component = this;
         return (
@@ -63,9 +94,9 @@ class WhiskyGrid extends Component {
                     <Table dark>
                         <thead>
                             <tr>
-                                <th className="per30">Name</th>                    
-                                <th className="per20">Age</th>
-                                <th className="per20">ABV</th>
+                                <th className="per30" onClick={() => this.sortBy('Prod_Name')}><FontAwesomeIcon icon={IconSort} />  Name</th>                    
+                                <th className="per20" onClick={() => this.sortBy('Prod_Age')}><FontAwesomeIcon icon={IconSort} />  Age</th>
+                                <th className="per20" onClick={() => this.sortBy('Prod_abv')}><FontAwesomeIcon icon={IconSort} />  ABV</th>
                                 <th className="per20"></th>  
                             </tr>            
                         </thead>
@@ -84,7 +115,7 @@ class WhiskyGrid extends Component {
                                 )
                             }                            
                         </tbody>
-                    </Table>
+                    </Table>    
                 </div>
             </div>
         );
